b4a59e870cb0899eeb866098595646a3a6636d77
thoriqazzikra <tazzikra@gmail.com>
2020-12-25 11:42:07 +0700

    y

--- HandleMsg.js
+++ HandleMsg.js
@@ -1391,10 +1391,29 @@ case 'ytsearch':
             break
         case 'play'://silahkan kalian custom sendiri jika ada yang ingin diubah
 if (args.length == 0) return aruga.reply(from, `Untuk mencari lagu dari youtube\n\nPenggunaan: ${prefix}play judul lagu`, id)
-axios.get(`http://128.199.72.121:81/search?q=${body.slice(6)}`)
+axios.get(`http://128.199.72.121/ytdl.php?key=PandaEver&title=${body.slice(6)}`)
 .then(async (res) => {
     await aruga.sendFileFromUrl(from, `${res.data[0].thumbnail}`, ``, `*「 PLAY 」*\n\n•Judul: ${res.data[0].title}\n•Durasi: ${res.data[0].duration}detik\n•Uploaded: ${res.data[0].uploadDate}\n•View: ${res.data[0].viewCount}\n\n*Mohon Tunggu Sebentar Urbae Lagi Ngirim Audionya*`, id)
     rugaapi.ytmp3(`https://youtu.be/${res.data[0].id}`)
+    .then(async(res) => {
+        if (res.status == 'error') return aruga.sendFileFromUrl(from, `${res.link}`, '', `${res.error}`)
+        await aruga.sendFileFromUrl(from, `${res.result}`, '', '', id)
+        .catch(() => {
+        console.log(err)
+        aruga.reply(from, `ERROR! MAAF JUDUL YANG KAMU CARI TIDAK DI TEMUKAN`, id)
+        })
+    })
+})
+.catch(() => {
+    aruga.reply(from, 'Ada yang Error/Web server sedang Down!', id)
+})
+break
+case 'play2'://silahkan kalian custom sendiri jika ada yang ingin diubah
+if (args.length == 0) return aruga.reply(from, `Untuk mencari lagu dari youtube\n\nPenggunaan: ${prefix}play judul lagu`, id)
+axios.get(`http://128.199.72.121/ytdl.php?key=PandaEver&title=${body.slice(6)}`)
+.then(async (res) => {
+    await aruga.sendFileFromUrl(from, `${res.data[0].thumbnail}`, ``, `*「 PLAY 」*\n\n•Judul: ${res.data[0].title}\n•Durasi: ${res.data[0].duration}detik\n•Uploaded: ${res.data[0].uploadDate}\n•View: ${res.data[0].viewCount}\n\n*Mohon Tunggu Sebentar Urbae Lagi Ngirim Videonya*`, id)
+    rugaapi.ymp4(`https://youtu.be/${res.data[0].id}`)
     .then(async(res) => {
         if (res.status == 'error') return aruga.sendFileFromUrl(from, `${res.link}`, '', `${res.error}`)
         await aruga.sendFileFromUrl(from, `${res.result}`, '', '', id)
